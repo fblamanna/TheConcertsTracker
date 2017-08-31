cf49542d99bd96945843bcc2481327cf9232cc15
Fabio Lamanna <fabio@fabiolamanna.it>
2017-08-31 14:19:03 +0200

    add TheSetlistsTracker.py

--- README.md
+++ README.md
@@ -45,13 +45,14 @@ You can run the code using the command:
 ```python TheConcertsTracker.py '5441c29d-3602-4898-b1a1-b77fa23b8e50' 'David Bowie' 'API_KEY'```
 
 ```
-ID,artist,eventdate,tourname,venue,venue_id,city,city_id,city_lat,city_lon,state,state_id,country,country_id
-3bd5f8b0,David Bowie,25-06-2004,A Reality Tour,Eichenring,33d6c80d,Scheeßel,2840070,53.1666667,9.4833333,Lower Saxony,06,Germany,DE
-13d2d1e1,David Bowie,23-06-2004,A Reality Tour,T-Mobile Arena,43d62b7f,Prague,3067696,50.0878367932108,14.4241322001241,Hlavní Mesto Praha,52,Czech Republic,CZ
-bd2d1e2,David Bowie,20-06-2004,A Reality Tour,Törnävä,2bd6d4ba,Seinäjoki,642149,62.5666667,23.0666667,Western Finland,15,Finland,FI
-3d2d1e3,David Bowie,18-06-2004,A Reality Tour,Frognerbadet,2bd628ce,Oslo,6942479,59.912697157616,10.7413673400879,Oslo,12,Norway,NO
-1bd2d1ec,David Bowie,17-06-2004,A Reality Tour,Ole Bergen Festival,43d64f17,Bergen,3161732,60.3931603404213,5.3242814540863,Hordaland,07,Norway,NO
-bd7250e,David Bowie,11-06-2004,A Reality tour,ArenA,5bd6379c,Amsterdam,2759794,52.373,4.9,North Holland,07,Netherlands,NL
+ID,artist;eventdate;tourname;venue;venue_id;city;city_id;city_lat;city_lon;state;state_id;country;country_id
+3bd5f8b0;David Bowie;25-06-2004;A Reality Tour;Eichenring;33d6c80d;Scheeßel;2840070;53.1666667;9.4833333;Lower Saxony;06;Germany;DE
+13d2d1e1;David Bowie;23-06-2004;A Reality Tour;T-Mobile Arena;43d62b7f;Prague;3067696;50.0878367932108;14.4241322001241;Hlavní Mesto Praha;52;Czech Republic;CZ
+bd2d1e2;David Bowie;20-06-2004;A Reality Tour;Törnävä;2bd6d4ba;Seinäjoki;642149;62.5666667;23.0666667;Western Finland;15;Finland;FI
+3d2d1e3;David Bowie;18-06-2004;A Reality Tour;Frognerbadet;2bd628ce;Oslo;6942479;59.912697157616;10.7413673400879;Oslo;12;Norway;NO
+1bd2d1ec;David Bowie;17-06-2004;A Reality Tour;Ole Bergen Festival;43d64f17;Bergen;3161732;60.3931603404213;5.3242814540863;Hordaland;07;Norway;NO
+bd7250e;David Bowie;11-06-2004;A Reality tour;ArenA;5bd6379c;Amsterdam;2759794;52.373;4.9;North Holland;07;Netherlands;NL
+...
 ```
 
 #### Version History
@@ -65,7 +66,53 @@ bd7250e,David Bowie,11-06-2004,A Reality tour,ArenA,5bd6379c,Amsterdam,2759794,5
 
 ***TheSetlistsTracker.py*** script script returns a .csv file with all information about the songs played in a concert for a single band or artist, available on the [setlist.fm](http://www.setlist.fm/) website. You need to apply for a **setlist.fm API key** to download data and use them; they are free for non-commercial projects. You can get it [here](http://api.setlist.fm/docs/index.html). Please read the [API Terms of Use](http://www.setlist.fm/help/terms) carefully.
 
-***Under Construction***
+#### Input
+
+The script takes as input the artist name (just for naming the output file) and the [Musicbrainz MBID](https://musicbrainz.org/doc/MusicBrainz_Database) code, which is an identifiable code for each artist or band in the database. I'm now working on an automatic call artist name - code. In the meantime you have to manually add the code available on the Musicbrainz website.
+
+##### Parameters:
+artistname: name of the artist or band (string)  
+artistcode: Musicbrainz MBID (string)  
+API_KEY: Setlist.fm valid API Key
+
+#### Output
+A .csv file with the following fields:
+
+1. **eventID** ID of the event
+2. **song** song played
+3. **set** type of set: 0 - Main Set; 1,2,3 etc. - Encore Number
+
+If a field information is missing, the code returns None for that value.
+
+#### Execution
+
+You can run the code using the command:  
+```$ python TheSetlistsTracker.py 'artistcode' 'artistname' 'API_KEY'```
+
+#### Sample Output
+```python TheSetlistsTracker.py '8538e728-ca0b-4321-b7e5-cff6565dd4c0' 'Depeche Mode' 'API_KEY'```
+
+```
+eventID;song;set
+73d7da3d;In Chains;0
+73d7da3d;Wrong;0
+73d7da3d;Hole to Feed;0
+73d7da3d;Walking in My Shoes;0
+73d7da3d;It's No Good;0
+73d7da3d;A Question of Time;0
+73d7da3d;Precious;0
+73d7da3d;Fly on the Windscreen;0
+73d7da3d;Home;0
+73d7da3d;Come Back;0
+73d7da3d;Peace;0
+73d7da3d;In Your Room;0
+73d7da3d;I Feel You;0
+73d7da3d;Enjoy the Silence;0
+73d7da3d;Never Let Me Down Again;0
+73d7da3d;Stripped;1
+73d7da3d;Personal Jesus;1
+...
+```
 
 ## Questions / Issues
 Contacts and Info: [www.fabiolamanna.it](http://www.fabiolamanna.it)
---
+++ TheSetlistsTracker.py
@@ -0,0 +1,221 @@
+# coding: utf8
+#
+# This Python script returns a .csv file with all information about the songs played in concerts by single bands or artists, 
+# available on the [setlist.fm](http://www.setlist.fm/) website.
+# You need to apply for a setlist.fm API key to download data and use them; they are free for non-commercial projects.
+# You can get it here: http://api.setlist.fm/docs/index.html. 
+# Please read the API Terms of Use (http://www.setlist.fm/help/terms) carefully.
+#
+# The algorithm takes as input the artist name (just for naming the output file) and 
+# the Musicbrainz MBID (https://musicbrainz.org/doc/MusicBrainz_Database) code,
+# which is an identifiable code for each artist or band in the database.
+# I'm now working on an automatic call artist name - code. 
+# In the meantime you have to manually add the code available on the Musicbrainz website.
+# 
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+__author__ = ''' Fabio Lamanna (fabio@fabiolamanna.it) '''
+#
+#########################################################################
+#
+# Copyright (c) 2017 Fabio Lamanna (fabio@fabiolamanna.it). 
+# Code under License GPLv3.
+#
+# Version History:
+# 22.07.2016 - Version 0.1
+# 27.12.2016 - Version 0.2 - Fixing bugs while reading .json
+# 31.08.2017 - Version 0.3 - Adding Compatibility with Setlist.fm API 1.0
+#
+# Input Parameters:
+# artistname: name of the artist or band (string)  
+# artistcode: Musicbrainz MBID (string)
+# API_KEY: Setlist.fm valid API Key
+#
+# Execution:
+# You can run the code using the command:  
+# $ python TheSetlistsTracker.py 'artistcode' 'artistname' 'API_KEY'
+#
+#########################################################################
+
+# Import Modules
+try:
+	import ujson as json
+except:
+	import json
+
+import requests
+import csv
+import sys
+
+#########################################################################
+
+def main():
+
+	# Set Workbooks for .csv
+	f = open(sys.argv[2] + 'SetlistsTracker.csv', 'wt')
+
+	# Write .csv Headers
+	writer = csv.writer(f, delimiter=';')
+
+	writer.writerow( 
+	                (
+	                'eventID',
+	                'song',
+	                'set'
+	                )
+	                )
+
+	# Call Setlist.fm API
+	url = 'https://api.setlist.fm/rest/1.0/artist/' + sys.argv[1] + '/setlists?p=1'
+	headers = {'Accept': 'application/json', 'x-api-key': sys.argv[3]}
+	r = requests.get(url, headers=headers)
+	
+	# Get .json Data
+	data = r.json()
+
+	# Get total number of shows
+	totalshows = int(data['total'])
+
+	# Total Number of Pages needed to load
+	pages = int(totalshows/20)
+
+	for page in range(1,pages):
+
+		url = 'https://api.setlist.fm/rest/1.0/artist/' + sys.argv[1] + '/setlists?p=' + str(page)
+		headers = {'Accept': 'application/json', 'x-api-key': sys.argv[3]}
+		r = requests.get(url, headers=headers)
+	
+		# Get .json Data
+		data = r.json()
+
+		for line in data:
+
+			# Loop through each setlist
+			for i in range(len(data['setlist'])):
+
+				# No Data Available
+				if data['setlist'][i]['sets'] == '':
+
+					writer.writerow(
+					                (
+					                # Event ID
+					                str(data['setlist'][i]['id']),
+					                # Songs
+					                'None',
+					                # Set
+					                'None'
+					                )
+					                )
+
+				else:
+
+					for j in data['setlist'][i]['sets']:
+
+						# Check for number of sets (Main, Encore(s))
+						numberofsets = len(data['setlist'][i]['sets']['set'])
+
+						if numberofsets == 1:
+
+							try:
+
+								for k in data['setlist'][i]['sets']['set']['song']:
+
+									try:
+
+										writer.writerow(
+										                (
+										                # Event ID
+										                str(data['setlist'][i]['id']),
+										                # Songs
+										                unicode(k['name']).encode('utf-8'),
+										                # Set
+										                '0'
+										                )
+										                )
+
+									except:
+
+										writer.writerow(
+										                (
+										                # Event ID
+										                str(data['setlist'][i]['id']),
+										                # Songs
+										                unicode(data['setlist'][i]['sets']['set']['song']['name']).encode('utf-8'),
+										                # Set
+										                '0'
+										                )
+										                )
+
+							except:
+
+								writer.writerow(
+								                (
+								                # Event ID
+								                str(data['setlist'][i]['id']),
+								                # Songs
+								                'None',
+								                # Set
+								                'None'
+								                )
+								                )
+
+						# Case numberofsets > 1
+						elif numberofsets > 1:
+
+							for s in range(numberofsets):
+
+								try:
+
+									for k in data['setlist'][i]['sets']['set'][s]['song']:
+
+										try:
+
+											writer.writerow(
+											                (
+											                # Event ID
+											                str(data['setlist'][i]['id']),
+											                # Songs
+											                unicode(k['name']).encode('utf-8'),
+											                # Set
+											                str(s)
+											                )
+											                )
+
+										except:
+
+											writer.writerow(
+											                (
+											                # Event ID
+											                str(data['setlist'][i]['id']),
+											                # Songs
+											                unicode(data['setlist'][i]['sets']['set'][s]['song']['name']).encode('utf-8'),
+											                # Set
+											                str(s)
+											                )
+											                )
+
+								except:
+
+									writer.writerow(
+									                (
+									                # Event ID
+									                str(data['setlist'][i]['id']),
+									                # Songs
+									                'None',
+									                # Set
+									                'None'
+									                )
+									                )
+
+	f.close()
+
+if __name__ == '__main__':
+
+	main()
+
+
+
+
